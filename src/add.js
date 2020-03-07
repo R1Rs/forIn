64618ac8bc3a34397167a4c40f365eef583e5462
Renat <suleymanov_renat@mail.ru>
2020-03-07 18:24:27 +0300

    task2

--- src/add.js
+++ src/add.js
@@ -1,9 +1,27 @@
-export default verificationSum;
+export default findBy;
 
-function verificationSum(sumBuy) {
-    let parsed = parseInt(sumBuy, 10);
-    if (isNaN(parsed)) throw ("введите число");
-    return parsed;
+function findBy(key, value) {
+  return person => person[key] === value;
 }
 
-    
+let finder = findBy("name", "маг");
+
+const results = [
+  {
+    name: "маг",
+    type: "character",
+    description: "Персонаж, обладающий магическими способностями"
+  },
+  {
+    name: "заклинание",
+    type: "attack",
+    description: "Атака магическим заклинанием"
+  },
+  {
+    name: "урон",
+    type: "help",
+    description: "Страница описания элемента интерфейса"
+  }
+].filter(finder);
+
+console.log(results);
--- test/test.js
+++ test/test.js
@@ -1,14 +1,35 @@
-import verificationSum from "../src/add";
+import findBy from "../src/add";
 
-test ("test number", () => {
-    const received = verificationSum("30");
-    const expected = 30;
+test ("test person", () => {
+    let finder = findBy("name", "маг");
 
-    const received1 = verificationSum("f");
-    const expected1 = Error;
+    const received = [
+      {
+        name: "маг",
+        type: "character",
+        description: "Персонаж, обладающий магическими способностями"
+      },
+      {
+        name: "заклинание",
+        type: "attack",
+        description: "Атака магическим заклинанием"
+      },
+      {
+        name: "урон",
+        type: "help",
+        description: "Страница описания элемента интерфейса"
+      }
+    ].filter(finder);
+
+    const expected = [
+      {
+        name: "маг",
+        type: "character",
+        description: "Персонаж, обладающий магическими способностями"
+      }
+    ];
 
     expect(received).toEqual(expected);
-    expect(received1).toEqual(expected1);
 })
 
 
